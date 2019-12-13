9cdb632ee5f41f735841888d8a92ae440ef994c8
Michael Builov <mbuilov@gmail.com>
2019-12-13 21:16:31 +0300

    make locale-independent

--- make_patch.sh
+++ make_patch.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-diff -rql orig/gawk-5.0.1 gawk-5.0.1 | grep -v "Only" | grep -v "/test/" | sed 's/^Files /diff -Naur /;s/ and / /;s/ differ$//' | sh
+LC_ALL=C diff -rql orig/gawk-5.0.1 gawk-5.0.1 | grep -v "Only" | grep -v "/test/" | sed 's/^Files /diff -Naur /;s/ and / /;s/ differ$//' | sh
